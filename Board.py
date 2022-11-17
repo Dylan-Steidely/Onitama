8fba8e4f0da5f51647eac984cdcc6b4a18b05b4d
Dylan-Steidely <115735113+Dylan-Steidely@users.noreply.github.com>
2022-11-16 21:24:04 -0500

    Art update and board

--- Board.py
+++ Board.py
@@ -1,3 +1,103 @@
 import pygame
 import sys
 pygame.init()
+class Board_tile:
+    """
+    0  1  2  3  4
+    5  6  7  8  9
+    10 11 12 13 14
+    15 16 17 18 19
+    20 21 22 23 24
+    this is an arrangement of how the tiles will look like.
+    each tile is given a tile_number which references its position so that it can be referenced
+    additionally there are 4 states a tile can be:
+    unseleceted which is the default for a tile
+    selected which is used for selecting a tile and also lets the user know where the 'cursor' is
+    possible unselcted tile is used to show a tile is an avilable move for the selected card
+    possible selected is a selceted tile that is a possible move
+    """
+
+    def __init__(self, game, tile_number,selected = False):
+        self.game = game #the name of the instance of the game class is inputted here
+        self.tile = tile_number #tile reference number
+        self.selected = selected #to know if tile is selected or not
+        self.possible = False #if tile is possible move
+        self.tile_image = pygame.image.load('images/unselectedtile.png') #unselected default image
+        self.tile_image_rect = self.tile_image.get_rect() #image rect
+
+
+    def _update_select(self):
+        # This method checks if tile is selceted and updates the image
+        if self.selected == True:
+            self.tile_image = pygame.image.load('images/selectedtile.png')
+        if self.selected == True and self.possible == True:
+            self.tile_image = pygame.image.load('images/selectedpossibletile.png')
+
+    def _update_possible(self):
+        #this method checks if tile is possible move and updates the image
+        if self.possible == True and self.selected != True:
+            self.tile_image = pygame.image.load('images/possibletile.png')
+
+    def arrange_tile(self):
+        #this method places tiles in their respective positions
+        if self.tile == 0:
+            self.game.screen.blit(self.tile_image,(0,0))
+            print('hello')
+        elif self.tile == 1:
+            self.game.screen.blit(self.tile_image, (130, 0))
+        elif self.tile == 2:
+            self.game.screen.blit(self.tile_image, (260, 0))
+        elif self.tile == 3:
+            self.game.screen.blit(self.tile_image, (390, 0))
+        elif self.tile == 4:
+            self.game.screen.blit(self.tile_image, (520, 0))
+        elif self.tile == 5:
+            self.game.screen.blit(self.tile_image,(0,130))
+        elif self.tile == 6:
+            self.game.screen.blit(self.tile_image, (130, 130))
+        elif self.tile == 7:
+            self.game.screen.blit(self.tile_image, (260, 130))
+        elif self.tile == 8:
+            self.game.screen.blit(self.tile_image, (390, 130))
+        elif self.tile == 9:
+            self.game.screen.blit(self.tile_image, (520, 130))
+        elif self.tile == 10:
+            self.game.screen.blit(self.tile_image, (0, 260))
+        elif self.tile == 11:
+            self.game.screen.blit(self.tile_image, (130, 260))
+        elif self.tile == 12:
+            self.game.screen.blit(self.tile_image, (260, 260))
+        elif self.tile == 13:
+            self.game.screen.blit(self.tile_image, (390, 260))
+        elif self.tile == 14:
+            self.game.screen.blit(self.tile_image, (520, 260))
+        elif self.tile == 15:
+            self.game.screen.blit(self.tile_image, (0, 390))
+        elif self.tile == 16:
+            self.game.screen.blit(self.tile_image, (130, 390))
+        elif self.tile == 17:
+            self.game.screen.blit(self.tile_image, (260, 390))
+        elif self.tile == 18:
+            self.game.screen.blit(self.tile_image, (390, 390))
+        elif self.tile == 19:
+            self.game.screen.blit(self.tile_image, (520, 390))
+        elif self.tile == 20:
+            self.game.screen.blit(self.tile_image, (0, 520))
+        elif self.tile == 21:
+            self.game.screen.blit(self.tile_image, (130, 520))
+        elif self.tile == 22:
+            self.game.screen.blit(self.tile_image, (260, 520))
+        elif self.tile == 23:
+            self.game.screen.blit(self.tile_image, (390, 520))
+        elif self.tile == 24:
+            self.game.screen.blit(self.tile_image, (520, 520))
+
+
+    def run_tiler(self):
+        #method combines the check and the arrnagement methods to make a tiler that when looped will tile the board
+        self._update_select()
+        self._update_possible()
+        self.arrange_tile()
+
+
+
--- game.py
+++ game.py
@@ -3,7 +3,9 @@ import pygame
 pygame.init()
 from cards import Card
 from cards import selected_cards_list
+from cards import select_cards
 from settings import Settings
+from Board import Board_tile
 
 class Game:
 
@@ -15,3 +17,67 @@ class Game:
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Onitama")
+
+    def run_game(self):
+        self._update_screen()
+        tile_0.run_tiler()
+        tile_1.run_tiler()
+        tile_2.run_tiler()
+        tile_3.run_tiler()
+        tile_4.run_tiler()
+        tile_5.run_tiler()
+        tile_6.run_tiler()
+        tile_7.run_tiler()
+        tile_8.run_tiler()
+        tile_9.run_tiler()
+        tile_10.run_tiler()
+        tile_11.run_tiler()
+        tile_12.run_tiler()
+        tile_13.run_tiler()
+        tile_14.run_tiler()
+        tile_15.run_tiler()
+        tile_16.run_tiler()
+        tile_17.run_tiler()
+        tile_18.run_tiler()
+        tile_19.run_tiler()
+        tile_20.run_tiler()
+        tile_21.run_tiler()
+        tile_22.run_tiler()
+        tile_23.run_tiler()
+        tile_24.run_tiler()
+
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+
+        pygame.display.flip()
+
+game = Game()
+tile_0 = Board_tile(game, 0)
+tile_1 = Board_tile(game, 1)
+tile_2 = Board_tile(game, 2)
+tile_3 = Board_tile(game, 3)
+tile_4 = Board_tile(game, 4)
+tile_5 = Board_tile(game, 5)
+tile_6 = Board_tile(game, 6)
+tile_7 = Board_tile(game, 7)
+tile_8 = Board_tile(game, 8)
+tile_9 = Board_tile(game, 9)
+tile_10 = Board_tile(game, 10)
+tile_11 = Board_tile(game, 11)
+tile_12 = Board_tile(game, 12)
+tile_13 = Board_tile(game, 13)
+tile_14 = Board_tile(game, 14)
+tile_15 = Board_tile(game, 15)
+tile_16 = Board_tile(game, 16)
+tile_17 = Board_tile(game, 17)
+tile_18 = Board_tile(game, 18)
+tile_19 = Board_tile(game, 19)
+tile_20 = Board_tile(game, 20)
+tile_21 = Board_tile(game, 21)
+tile_22 = Board_tile(game, 22)
+tile_23 = Board_tile(game, 23)
+tile_24 = Board_tile(game, 24)
+
+game.run_game()
+
